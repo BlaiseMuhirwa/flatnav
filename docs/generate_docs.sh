27f465e72815b0c5d5e696d6a2a88b1a714a7ffb
blaise-muhirwa <blaise@groundlight.ai>
2024-04-08 20:22:20 -0700

    make sure we're also generating cpp docs

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -13,5 +13,18 @@ cd ../docs
 WHEEL_FILE=$(ls ../flatnav_python/dist/*.whl)
 
 poetry run pip install ${WHEEL_FILE} 
+
+# Build the doxygen documentation for the C++ code. 
+# First, we need to install doxygen if it is not already installed
+if ! [ -x "$(command -v doxygen)" ]; then
+    echo "doxygen is not installed. Installing doxygen"
+    sudo apt-get update
+    sudo apt-get install doxygen
+fi
+
+# Now we can build the documentation
+doxygen Doxyfile 
+
+# Now onto the sphinx documentation
 poetry run make clean 
 poetry run make html 
