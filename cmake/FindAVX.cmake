a398feb6691246694da5cc1612f2d6002c415498
BlaiseMuhirwa <blaisemuhirwa3@gmail.com>
2024-12-04 05:49:53 -0800

    refactoring cmake setup

--- cmake/FindAVX.cmake
+++ cmake/FindAVX.cmake
@@ -1,58 +1,52 @@
 cmake_minimum_required(VERSION 3.14 FATAL_ERROR)
 
-
 if(CMAKE_SYSTEM_NAME MATCHES "Linux")
-  EXEC_PROGRAM(cat ARGS "/proc/cpuinfo" OUTPUT_VARIABLE CPUINFO)
+  execute_process(COMMAND cat /proc/cpuinfo OUTPUT_VARIABLE CPUINFO OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-  STRING(REGEX MATCH ".*\\ssse.*" SSE_FOUND ${CPUINFO})
-  STRING(REGEX MATCH ".*\\ssse3.*" SSE3_FOUND ${CPUINFO})
-  STRING(REGEX MATCH ".*\\avx.*" AVX_FOUND ${CPUINFO})
-  STRING(REGEX MATCH ".*\\avx2.*" AVX2_FOUND ${CPUINFO})
-  STRING(REGEX MATCH ".*\\avx512.*" AVX512_FOUND ${CPUINFO})
+  string(FIND "${CPUINFO}" "sse" SSE_FOUND)
+  string(FIND "${CPUINFO}" "sse3" SSE3_FOUND)
+  string(FIND "${CPUINFO}" "avx" AVX_FOUND)
+  string(FIND "${CPUINFO}" "avx2" AVX2_FOUND)
+  string(FIND "${CPUINFO}" "avx512" AVX512_FOUND)
 
-  if(SSE_FOUND)
+  if(SSE_FOUND GREATER -1)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse")
     message(STATUS "Building with SSE")
   endif()
 
-  if(SSE3_FOUND)
+  if(SSE3_FOUND GREATER -1)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse3")
     message(STATUS "Building with SSE3")
   endif()
 
-  if(AVX_FOUND)
+  if(AVX_FOUND GREATER -1)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mavx")
     message(STATUS "Building with AVX")
   endif()
 
-  if(AVX2_FOUND)
+  if(AVX2_FOUND GREATER -1)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mavx2")
     message(STATUS "Building with AVX2")
   endif()
 
-  if(AVX512_FOUND)
+  if(AVX512_FOUND GREATER -1)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mavx512f -mavx512dq -mavx512vl -mavx512bw -mavx512vnni")
     message(STATUS "Building with AVX512")
   endif()
 
 elseif(CMAKE_SYSTEM_NAME MATCHES "Darwin")
-  EXEC_PROGRAM("/usr/sbin/sysctl -n machdep.cpu.features" OUTPUT_VARIABLE CPUINFO)
+  execute_process(COMMAND /usr/sbin/sysctl -n machdep.cpu.features OUTPUT_VARIABLE CPUINFO OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-  STRING(REGEX REPLACE "^.*[^S](SSE).*$" "\\1" SSE_FOUND ${CPUINFO})
-  STRING(REGEX REPLACE "^.*[^S](SSE3).*$" "\\1" SSE3_FOUND ${CPUINFO})
+  string(FIND "${CPUINFO}" "SSE" SSE_FOUND)
+  string(FIND "${CPUINFO}" "SSE3" SSE3_FOUND)
 
-  STRING(COMPARE EQUAL "SSE" ${SSE_FOUND} SSE_FOUND)
-  STRING(COMPARE EQUAL "SSE3" ${SSE3_FOUND} SSE3_FOUND)
-
-  if(SSE_FOUND)
+  if(SSE_FOUND GREATER -1)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse")
     message(STATUS "Building with SSE")
   endif()
 
-  if(SSE3_FOUND)
+  if(SSE3_FOUND GREATER -1)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse3")
-    message(STATUS "Building with SSE2")
+    message(STATUS "Building with SSE3")
   endif()
-
-
 endif()
